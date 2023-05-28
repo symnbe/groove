f42e8e366db05b285b59c5f5ade4d5f16807fb7d
symnbe <ookaminingen@me.com>
2023-05-28 14:00:59 +0900

    add/posts/new.html

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,29 @@
 class PostsController < ApplicationController
   def new
+    @post = Post.new
+  end
+  
+  def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new
+    end
   end
 
   def index
+    @posts = Post.all
   end
 
   def edit
   end
+  
+  private
+  
+  def post_params
+    params.require(:post).permit(:title, :url)
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
  
   devise_for :users
   root to:"homes#top"
-  resources:posts, only:[:new, :index, :edit, :update, :destroy]
+  resources:posts, only:[:new, :create, :index, :edit, :update, :destroy]
 end
