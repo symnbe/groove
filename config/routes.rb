ce575ef934d6abc0e8576d7ae36843d9075adcdb
symnbe <ookaminingen@me.com>
2023-05-29 17:04:01 +0000

    [fix[index.html/editボタンの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   
  
+  get 'user/show'
+  get 'user/edit'
   devise_for :users
   root to:"homes#top"
   resources:posts, only:[:new, :create, :index, :edit, :update, :destroy]
