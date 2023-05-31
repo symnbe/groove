a8dc94fbb82cc47baf7c281c52f85160fc5d9f9c
symnbe <ookaminingen@me.com>
2023-05-31 09:03:25 +0000

    save

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   get 'user/edit'
   devise_for :users
   root to:"homes#top"
-  resources:posts, only:[:new, :create, :index, :edit, :update, :destroy]
+  resources:posts
 end
