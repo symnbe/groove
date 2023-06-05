7c225dc8c7a6cbc8b147d9d35862c3656593a28c
symnbe <ookaminingen@me.com>
2023-06-05 05:41:58 +0000

    [saved]users/show.html/tabの設置（タブが縦並びの為次回修正）

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,17 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @posts = @user.posts.all
   end
 
   def edit
   end
-  
+
   private
-  
+
   def
   user_params
+  params.require(:user).permit(:name)
   end
-  
+
 end
