b560b7a9662983067abaacdb327986763cfca8f8
symnbe <ookaminingen@me.com>
2023-06-12 07:11:09 +0000

    [saved]posts/posts_controller.rb/

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -40,7 +40,7 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:title, :url, :category)
+    params.require(:post).permit(:title, :url)
   end
 
 end
