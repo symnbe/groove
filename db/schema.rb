0879b15621cf462817d127a8195ca36c136ebdda
symnbe <ookaminingen@me.com>
2023-06-23 01:33:31 +0000

    [saved]users/edit.html

--- db/schema.rb
+++ db/schema.rb
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 2023_05_29_023656) do
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.string "name"
+    t.text "introduce"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
